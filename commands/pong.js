32ecd83602f05b80dd892cd820a2d34918c747c9
Sam772 <78389553+Sam772@users.noreply.github.com>
2021-10-17 16:57:53 +0100

    Delete pong.js

--- commands/pong.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'pong',
-    description: "tests if command works",
-
-    execute(client, message, arguments, distube, Discord) {
-        message.channel.send(':ping_pong:');
-    }
-}
